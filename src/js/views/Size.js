5ff16c4e6feb09e768f439a8f0d65318fb88a640
pointofpresence <github@pointofpresence.ru>
2015-06-07 13:47:49 +0600

    Size view

---
+++ src/js/views/Size.js
@@ -0,0 +1,31 @@
+define("views/Size", [
+    "backbone",
+    "jquery"
+], function (Backbone, $) {
+    "use strict";
+
+    return Backbone.View.extend({
+        el: "#sizes",
+
+        events: {
+            "click button":     "open",
+            "click .btn-block": "toggle"
+        },
+
+        open: function (e) {
+            e.preventDefault();
+
+            this.model.set({
+                size: parseInt($(e.currentTarget).data("value"))
+            });
+        },
+
+        toggle: function (e) {
+            e.preventDefault();
+
+            this.model.set({
+                fullwidth: !this.model.get("fullwidth")
+            });
+        }
+    });
+});
