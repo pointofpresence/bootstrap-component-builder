9f1a3c128e49a59e0ddd5c867bcc655714205b11
pointofpresence <github@pointofpresence.ru>
2015-06-14 16:56:35 +0600

    1.0.31

--- dist/js/tpl.js
+++ dist/js/tpl.js
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2015 pointofpresence
  * bootstrap-component-builder - Online component builder for Bootstrap 3
- * @version v1.0.30
+ * @version v1.0.31
  * @link https://github.com/pointofpresence/bootstrap-component-builder
  * @license MIT
  */
